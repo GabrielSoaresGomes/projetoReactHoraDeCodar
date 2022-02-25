ef136e315963426c2f75822d757b4dc93ed28e16
Gabriel Soares gomes <dev.gabrielsoares221@gmail.com>
2022-02-25 18:40:55 -0300

    feat: Iniciando os arquivos layouts

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,23 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 
+import Container from './components/layout/Container';
 
 function App() {
   return (
     <Router>
       <ul>
-        <li>Home</li>
-        <li>Contato</li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/company">Empresa</Link></li>
+        <li><Link to="/contact">Contato</Link></li>
+        <li><Link to="/newproject">Novo Projeto</Link></li>
       </ul>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/company" element={<Company/>} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/newproject" element={<NewProject/>} />
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/company" element={<Company/>} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="/newproject" element={<NewProject/>} />
+        </Routes>
+      </Container>
     </Router>
   );
 }
