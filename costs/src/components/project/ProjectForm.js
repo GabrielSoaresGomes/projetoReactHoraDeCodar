4fe10d06508418ff100ce2cd30250240acfc43f6
Gabriel Soares gomes <dev.gabrielsoares221@gmail.com>
2022-03-05 11:18:17 -0300

    feat: Utilizando o hook para funcionar,e corrigindo o fetch

--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -1,4 +1,4 @@
-import {useState} from "react"
+import {useEffect, useState} from "react"
 
 import styles from "./ProjectForm.module.css"
 
@@ -9,27 +9,29 @@ import SubmitButton from "../form/SubmitButton.js"
 function ProjectForm({btnText}) {
     const [categories, setCategories] = useState([])
 
-    //Fazendo um request
-    fetch("https://localhost:5000/categories", {
-        method: "GET",
-        headers: {
-            "Content-Type": "applications/json"
-        }
-    })
-        //Pegando os valores do json
-        .then((resp) => resp.json())
-        //Inserindo os valores do json em um array com useStates
-        .then((data) => {
-            setCategories(data)
+    useEffect(() => {
+            //Fazendo um request
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                "Content-Type": "applications/json"
+            },
         })
-        //Um log de algum possível erro
-        .catch(err => console.log(err))
+            //Pegando os valores do json
+            .then((resp) => resp.json())
+            //Inserindo os valores do json em um array com useStates
+            .then((data) => {
+                setCategories(data)
+            })
+            //Um log de algum possível erro
+            .catch(err => console.log(err))
+    },[]) 
 
     return (
        <form className = {styles.form}>
                <Input text={"Insira o nome do projeto"} name={"Nome do projeto"} type={'text'} placeholder={"Digite aqui..."} />
                <Input text={"Insira o orçamento total"} name={"Orçamento total"} type={'number'} placeholder={"Digite aqui..."} />
-           <Select name="category_id" text="Selecione a categoria" />
+           <Select name="category_id" options={categories} text="Selecione a categoria" />
            <SubmitButton text={btnText} />
        </form>
     )
