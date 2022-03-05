a9ce2edaa7b4be2d52f20ab13b4a473e748fc915
Gabriel Soares gomes <dev.gabrielsoares221@gmail.com>
2022-03-05 11:07:53 -0300

    feat: Realizando um request com API de um arquivo json

--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -1,3 +1,5 @@
+import {useState} from "react"
+
 import styles from "./ProjectForm.module.css"
 
 import Input from "../form/Input.js"
@@ -5,6 +7,24 @@ import Select from "../form/Select.js"
 import SubmitButton from "../form/SubmitButton.js"
 
 function ProjectForm({btnText}) {
+    const [categories, setCategories] = useState([])
+
+    //Fazendo um request
+    fetch("https://localhost:5000/categories", {
+        method: "GET",
+        headers: {
+            "Content-Type": "applications/json"
+        }
+    })
+        //Pegando os valores do json
+        .then((resp) => resp.json())
+        //Inserindo os valores do json em um array com useStates
+        .then((data) => {
+            setCategories(data)
+        })
+        //Um log de algum possível erro
+        .catch(err => console.log(err))
+
     return (
        <form className = {styles.form}>
                <Input text={"Insira o nome do projeto"} name={"Nome do projeto"} type={'text'} placeholder={"Digite aqui..."} />
