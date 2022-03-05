d1bf2fc846f1b677789a847da8ec9a1d782bf510
Gabriel Soares gomes <dev.gabrielsoares221@gmail.com>
2022-03-05 10:50:23 -0300

    feat: Finalizando os componentes essenciais do form

--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -1,18 +1,16 @@
 import styles from "./ProjectForm.module.css"
 
 import Input from "../form/Input.js"
+import Select from "../form/Select.js"
+import SubmitButton from "../form/SubmitButton.js"
 
-function ProjectForm() {
+function ProjectForm({btnText}) {
     return (
        <form className = {styles.form}>
                <Input text={"Insira o nome do projeto"} name={"Nome do projeto"} type={'text'} placeholder={"Digite aqui..."} />
                <Input text={"Insira o orçamento total"} name={"Orçamento total"} type={'number'} placeholder={"Digite aqui..."} />
-           <div>
-               <select name="category_id">
-                <option disabled>Selecione a categoria</option>
-           </select>
-           </div>
-           <Input name={"Salvar Projeto"} type={'submit'} placeholder={"Salvar Projeto"} />        
+           <Select name="category_id" text="Selecione a categoria" />
+           <SubmitButton text={btnText} />
        </form>
     )
 }
